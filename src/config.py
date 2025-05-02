8e0c7ccd96d20d53707408267211978ddf0c6b02
Edwin Hartarto <edwin.hartarto91@gmail.com>
2025-05-02 08:19:17 +0000

    the recent neo4J configuration of auraDB

--- src/config.py
+++ src/config.py
@@ -1,4 +1,11 @@
 """Configuration parameters"""
 from neomodel import config
 
-config.DATABASE_URL = "bolt://neo4j:password@localhost:7687/spotify"
+# config.DATABASE_URL = "bolt://neo4j:password@localhost:7687/spotify"
+config.DATABASE_URL = "neo4j+s://neo4j:xrFQs_T9cAuMMFwJ40sqq7KoLz8-E4gLIs8Yb_eb6rw@3495cdd3.databases.neo4j.io"
+
+NEO4J_URI="neo4j+s://3495cdd3.databases.neo4j.io"
+NEO4J_USERNAME="neo4j"
+NEO4J_PASSWORD="xrFQs_T9cAuMMFwJ40sqq7KoLz8-E4gLIs8Yb_eb6rw"
+AURA_INSTANCEID="3495cdd3"
+AURA_INSTANCENAME="Instance01"
